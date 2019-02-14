578486fe6c59b7e2f16889d8286c317ed36318c8
rex0625 <rex0625@gmail.com>
2019-02-14 17:01:50 +0800

    Update receive.php

--- receive.php
+++ receive.php
@@ -13,7 +13,7 @@
     $ch = curl_init($url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-        'Authorization: Bearer gd1gyH+Pc5TROu9ku5u/5tDvFnffsU8nXU69zXuhTgE0dIS5nVGmx9Js8PwijeUqgFuwWXzyJ14/N5FUmp/UXsmSJbUsxMGA6AW1gozlf6cbEgSGLiC02BEaRa5wUSqE7df8FOANP1WjPW8Mh/TgtwdB04t89/1O/w1cDnyilFU'
+        'Authorization: Bearer aMMZMPEtoNH2HGgjnH3YyHMoVYp61TondibRuL2i5M1Fip9qg7mbmru3t9ARRkIkWI4ack9s7OtsyWW9/WhN3ciMEoXp7chGIgLirdmMjfgX6y0N2lwLRZzHpDPBQV1jFLG9OA9lpQeS/q/YqY77LwdB04t89/1O/w1cDnyilFU='
     ));
 
     $json_content = curl_exec($ch);
@@ -71,7 +71,7 @@
   
     fwrite($myfile, "\xEF\xBB\xBF".json_encode($response)); //在字串前面加上\xEF\xBB\xBF轉成utf8格式
     $header[] = "Content-Type: application/json";
-    $header[] = "Authorization: Bearer gd1gyH+Pc5TROu9ku5u/5tDvFnffsU8nXU69zXuhTgE0dIS5nVGmx9Js8PwijeUqgFuwWXzyJ14/N5FUmp/UXsmSJbUsxMGA6AW1gozlf6cbEgSGLiC02BEaRa5wUSqE7df8FOANP1WjPW8Mh/TgtwdB04t89/1O/w1cDnyilFU";
+    $header[] = "Authorization: Bearer aMMZMPEtoNH2HGgjnH3YyHMoVYp61TondibRuL2i5M1Fip9qg7mbmru3t9ARRkIkWI4ack9s7OtsyWW9/WhN3ciMEoXp7chGIgLirdmMjfgX6y0N2lwLRZzHpDPBQV1jFLG9OA9lpQeS/q/YqY77LwdB04t89/1O/w1cDnyilFU=";
     $ch = curl_init("https://api.line.me/v2/bot/message/reply");
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
     curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));                                                                  
