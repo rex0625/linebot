606af789195a44446689018ff2da5ec2108ba7eb
rex0625 <rex0625@gmail.com>
2019-02-14 10:20:34 +0800

    Update receive.php

--- receive.php
+++ receive.php
@@ -1,7 +1,7 @@
 <?php
-$json_str = file_get_contents('php://input');
-$json_object = json_decode($json_str);
+  $json_str = file_get_contents('php://input');
+  $json_object = json_decode($json_str);
 
-$myfile = fopen("log.txt","w+") or die("unable open file");
-fwrite($myfile,"\xEF\xBB\xBF".$json_str);
+  $myfile = fopen("log.txt","w+") or die("unable open file");
+  fwrite($myfile,"\xEF\xBB\xBF".$json_str);
 ?>
